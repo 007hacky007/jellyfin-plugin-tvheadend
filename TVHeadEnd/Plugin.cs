29c9efe396e45cc7be9bbbfee92868877b366526
007hacky007 <007hacky007@users.noreply.github.com>
2026-08-22 23:13:57 +0200

    Fork branding: TVHeadend Fail-Fast 15.1.0.0

--- TVHeadEnd/Plugin.cs
+++ TVHeadEnd/Plugin.cs
@@ -13,7 +13,7 @@ namespace TVHeadEnd
     /// </summary>
     public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
-        private static readonly Guid _pluginId = new Guid("3fd018e5-5e78-4e58-b280-a0c068febee0");
+        private static readonly Guid _pluginId = new Guid("980dc09b-7127-4a6e-b101-b6ae374ea0cc");
 
         public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
             : base(applicationPaths, xmlSerializer)
@@ -31,13 +31,13 @@ namespace TVHeadEnd
         /// Gets the name of the plugin.
         /// </summary>
         /// <value>The name.</value>
-        public override string Name => "TVHeadend";
+        public override string Name => "TVHeadend Fail-Fast";
 
         /// <summary>
         /// Gets the description.
         /// </summary>
         /// <value>The description.</value>
-        public override string Description => "Provides live TV using TVHeadend as the source.";
+        public override string Description => "Provides live TV using TVHeadend as the source. Fork of the official plugin that stays responsive when the TVHeadend server is unreachable.";
 
         /// <inheritdoc />
         public override Guid Id => _pluginId;
